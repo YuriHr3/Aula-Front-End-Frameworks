395e54a77675dfea1d26b1b0f4982fc17a9200b1
Yuri <141838403+YuriHr3@users.noreply.github.com>
2025-09-10 10:58:48 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+let numero = 10
+let numeroDois = 20
+let media = numero + numeroDois
+let resultado = (media / 2)
+console.log('a media é:', resultado)
+
+let arrayNome = ['Yuri', 'Emilly', 'Yuri Black']
+let arrayNumero = [1, 2, 3, 4, 5]
+console.log(arrayNome[2])
+const objtNome = {
+    nome: 'Yuri',
+    sobre: 'Emilly',
+    idade: true
+}
+console.log(objtNome.sobre)
+let a = 4
+let b = 1
+
+if (a + b == 4) {
+    console.log('É o 4 prr')
+} else {
+    console.log('4 é o  krlh, aqui é tudo 2 ')
+}
+
+let f = 1
+let x = 1
+switch (f + x) {
+    case 0:
+        console.log('valor 0')
+        break
+    case 1:
+        console.log('valor 1')
+        break
+    case 2:
+        console.log('valor 2')
+        break
+    default:
+        console.log('nao entrou nao case')
+        break;
+
+}
